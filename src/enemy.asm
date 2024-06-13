0cc9a8206af894b3bb82ed0205f90c82beec2626
thegoodstuffshack <thegoodstuffshack@gmail.com>
2024-06-13 22:50:25 +1000

    remove unused rng code

--- src/enemy.asm
+++ src/enemy.asm
@@ -74,10 +74,4 @@ enemy_memory: times enemy_count db 0
 enemy_list: db           0,      0,      0
 enemy_positions: dw coord(1,1), coord(10,10), coord(15, 2)
 
-
-; enemy_rng_pointer: dw 0xFFFF
-; enemy_rng_count equ 20
-; enemy_rng:
-; db 1, 2, 1, 3, 1, 2, 3, 2, 1, 2, 3, 1, 1, 1, 3, 2, 1, 1, 2, 3
-
 %include "src/enemies/stalker.asm"
