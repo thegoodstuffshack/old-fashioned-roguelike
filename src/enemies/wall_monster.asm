b0a3836328b89f40c95a3aa87550e543c5f15095
thegoodstuffshack <thegoodstuffshack@gmail.com>
2024-07-26 17:34:45 +1000

    start to work on wall enemy

--- src/enemies/wall_monster.asm
+++ src/enemies/wall_monster.asm
@@ -1,13 +1,37 @@
 ;
 ; wall monster, spawns and moves along walls (in the wall), when player gets close, attack
 
+[bits 16]
+
 
 wall_monster:
 	push bp
 	mov bp, sp
 
-	
+	mov dh, dl
+	mov ch, cl
 
+	xor ah, ah
+
+.check_for_players:
+	inc ah
+
+.top_right:
+; (y+r), 
+; (y-1)*(r-1), (x+1)*(r-1), 
+; (y+1)(x-1), 
+; (x-1)*(r-3), (y+1)*(r-3),
+; (y-1)(x+1)*(r-4),
+; (x-1)
+
+
+
+
+.bottom_right:
+
+.bottom_left:
+
+.top_left:
 
 
 .end:
--- src/enemy.asm
+++ src/enemy.asm
@@ -76,3 +76,4 @@ enemy_list: db               0,            0,            0,           1
 enemy_positions: dw coord(1,1), coord(10,10), coord(15, 2), coord(1, 0)
 
 %include "src/enemies/stalker.asm"
+%include "src/enemies/wall_monster.asm"
